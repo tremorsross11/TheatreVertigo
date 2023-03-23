d41afba0265b31771423f268d8c3420a422d22b6
tremorsross11 <118317710+tremorsross11@users.noreply.github.com>
2023-03-23 13:46:28 -0700

    Add files via upload

---
+++ Helpers/TextHelpers.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace TheatreCMS3.Helpers
+{
+    public static class TextHelpers
+    {
+        public static string Truncate(this string value, int maxLength)
+        {
+            // Check for valid string first
+            if (string.IsNullOrEmpty(value)) return value;
+
+            // If value is valid string, then proceed with method
+            var newString = string.Empty;
+            if (value.Length > maxLength)
+            {
+                newString = value.Substring(0, maxLength) + "...";
+            }
+            else
+            {
+                newString = value;
+            }
+            return newString;
+        }
+
+        public static string LimitWords(string value, int maxWords)
+        {
+            // if string is null or empty, return string value
+            if (string.IsNullOrEmpty(value)) return value;
+
+            int wordCount = 0, index = 0;
+
+            // skip whitespace until first word
+            while (index < value.Length && char.IsWhiteSpace(value[index]))
+                index++;
+
+            while (index < value.Length)
+            {
+                // check if current char is part of a word
+                while (index < value.Length && !char.IsWhiteSpace(value[index]))
+                    index++;
+
+                wordCount++;
+
+                // skip whitespace until next word
+                while (index < value.Length && char.IsWhiteSpace(value[index]))
+                    index++;
+            }
+
+            var newWords = string.Empty;
+
+            // if max words is set to 0 or less than 0, return string value
+            if (maxWords < 0 || maxWords == 0) return value;
+
+            // if word count is greater than max words, get max words
+            if (wordCount > maxWords)
+            {
+                // Create list based on number of words (Take(maxwords))
+                // Split on spaces, using RemoveEmptyEntries to ignore spaces in collecting words
+                List<string> myWordsList = value.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries).Take(maxWords).ToList();
+                // Create string of words from the list, seperated by spaces
+                string pickedWords = string.Join(" ", myWordsList);
+                // Add ellipses to the end of the string
+                newWords = pickedWords + "...";            }
+            else
+            {
+                newWords = value;
+            }
+
+            return newWords;
+        }
+    }
+}
---
+++ Models/IdentityModels.cs
@@ -0,0 +1,59 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using TheatreCMS3.Areas.Rent.Models;
+
+namespace TheatreCMS3.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public ApplicationDbContext(string nameOrConnectionString) : base(nameOrConnectionString)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+
+
+
+        /* ▼ Put DbSet's for your models below ▼ */
+        public DbSet<RentalHistory> RentalHistories { get; set; }
+
+        public System.Data.Entity.DbSet<TheatreCMS3.Areas.Prod.Models.ProductionPhotos> ProductionPhotos { get; set; }
+    
+        public DbSet<Areas.Prod.Models.CastMember> CastMembers { get; set; }
+
+        public System.Data.Entity.DbSet<TheatreCMS3.Areas.Prod.Models.Production> Productions { get; set; }
+
+        public System.Data.Entity.DbSet<TheatreCMS3.Areas.Prod.Models.CalendarEvent> CalendarEvents { get; set; }
+    }
+
+
+}
